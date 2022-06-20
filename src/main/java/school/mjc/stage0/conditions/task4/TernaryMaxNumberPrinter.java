55400c4ef990d4b2e101cb4eee681dc12bf56fe7
Andrei Ozerov <andrei.ozerov@gmail.com>
2022-06-20 22:25:59 +0300

    Task #4. Ternary Conditions Done

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int comparisonFirstTwo = (first > second) ? first : second;
+        int result = (comparisonFirstTwo > third) ? comparisonFirstTwo : third;
+        System.out.println(result);
     }
 }
